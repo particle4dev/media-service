3f437aab63980d4b71c80d8105b3346397d098de
hoang <particle4dev@lehoangs-MacBook-Pro.local>
2017-08-09 17:07:08 +0700

    Use terraform to create API Gateway and link it to Lambda Function

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,6 +3,9 @@ variable "secret_key" {}
 variable "region" {
   default = "ap-southeast-1"
 }
+variable "accountId" {
+  default = "799791150485"
+}
 variable "subdomain" {
   default = "media.particle4dev.com"
 }
