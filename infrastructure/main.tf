9647d67212b10c77eb5039097f9ed46e43b82169
Hoang Nam <particle4dev@gmail.com>
2017-08-10 17:51:59 +0000

    update api gateway

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -105,7 +105,7 @@ resource "aws_api_gateway_resource" "HelloWorldResource" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_resource.HelloWorldResource.id}"
-  http_method   = "POST"
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
@@ -125,6 +125,17 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}/helloworldresource"
+  # source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}/helloworldresource"
+  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/*/*"
+}
 
+resource "aws_api_gateway_deployment" "MyDemoDeployment" {
+  depends_on = ["aws_api_gateway_method.method"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "test"
+
+  variables = {
+    "version" = "1.0.0"
+  }
 }
